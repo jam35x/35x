c32d1bdfca26a8200ab8f6cb79147fca65f005eb
James <39327146+jam35x@users.noreply.github.com>
2020-03-27 08:39:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 var mypoolid = "3c6b71eb0ee0152b8b2ded843539c3f39c9fcaa74df987e968cf2388f2392bea";
 let LoadData = function() {
     const start = Date.now();
-    $.getJSON( "https://pooltool.s3-us-west-2.amazonaws.com/8e4d2a3/pools/"+mypoolid+"/livestats.json?now="+String(start), function( data ) {
-        var formattedstake = numeral(data["livestake"]/1000000).format('0.00a');
-
-        $("#ptstake").html(formattedstake);
-        $("#pteblocks").html(data["epochblocks"]);
-    });
-
+    var currentepoch = 0;
     $.getJSON( "https://pooltool.s3-us-west-2.amazonaws.com/stats/stats.json?now="+String(start), function( data ) {
         // var formattedheight = numeral(data['majoritymax']).format('0');
         $("#ptepoch").html(data["currentepoch"]);
+        currentepoch = data["currentepoch"];
         $("#ptslot").html(data["currentslot"]);
         $("#ptheight").html(data["majoritymax"]);
+    });
+
+    $.getJSON( "https://pooltool.s3-us-west-2.amazonaws.com/8e4d2a3/pools/"+mypoolid+"/livestats.json?now="+String(start), function( data ) {
+        var formattedstake = numeral(data["livestake"]/1000000).format('0.00a');
+
+            $("#ptstake").html(formattedstake);
+            if (
+                typeof(data["lastBlockEpoch"])=="undefined" ||
+                parseInt(currentepoch) != parseInt(data["lastBlockEpoch"]) &&
+                ! parseInt(currentepoch) == (parseInt(data["lastBlockEpoch"]) + 1)
+            )  {
+                data["epochblocks"]="?"
+                data["lifetimeblocks"]="?"
+            } else if (parseInt(currentepoch) == (parseInt(data["lastBlockEpoch"]) + 1)) {
+                data["epochblocks"]="0"
+            };
+            $("#pteblocks").html(data["epochblocks"]);
+            $("#ptlblocks").html(data["lifetimeblocks"]);
     });
 }
 
