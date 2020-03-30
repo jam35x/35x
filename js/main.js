300f86816b66558c91fbd34bf1c2e8b075c91dd6
James <39327146+jam35x@users.noreply.github.com>
2020-03-30 09:56:21 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -108,4 +108,8 @@ $(document).ready(function() {
             $(this).collapse('hide');
         }
     });
+    $(".navbar-nav .nav-link").click(function(){
+        $(".navbar-nav .nav-link").removeClass('active');
+        $(this).addClass('active');
+    });
 });
