f37957a653867030e28f4649cb76d76c9d1e923f
James <39327146+jam35x@users.noreply.github.com>
2020-06-22 14:29:25 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ let LoadData = function() {
 };
 
 let StakeData = function() {
-    $.getJSON( "https://pooltool.s3-us-west-2.amazonaws.com/8e4d2a3/pools/"+mypoolid+"/epochstats.json", function( data ) {
+    $.getJSON( "epochstats.json", function( data ) {
         $.each(data, function(index, jsonObject){
             if (index == "updatedAt") {
                 return false;
